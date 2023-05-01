73c238c9bbd868fd8677dc63a9f31ef3d13366aa
iuriid <iurii.dziuban@gmail.com>
2023-05-01 11:17:40 +0300

    refactor the embeddings script

--- embeddings/generate-upload-vectors.py
+++ embeddings/generate-upload-vectors.py
@@ -1,20 +1,21 @@
+import os
 import tiktoken
 import openai
 import pinecone
 
-import mysecrets
+import keys
 
 tokenizer = tiktoken.get_encoding("gpt2")
 
-openai.organization = mysecrets.OPENAI_ORG
-openai.api_key = mysecrets.OPENAI_KEY
+openai.organization = keys.OPENAI_ORG
+openai.api_key = keys.OPENAI_KEY
 
-PINECONE_INDEX_NAME = 'scheppach'
+PINECONE_INDEX_NAME = keys.PINECONE_INDEX_NAME
 MODEL = "text-embedding-ada-002"
-INDEX_DIMENSIONS = 1536
+INDEX_DIMENSIONS = 1536 # specific for "text-embedding-ada-002" model
 
 pinecone.init(
-    api_key = mysecrets.PINECONE_API_KEY,
+    api_key = keys.PINECONE_API_KEY,
     environment = 'us-east1-gcp'
 )
 
@@ -22,50 +23,38 @@ pinecone.init(
 print('Pinecone Indexes: ', pinecone.list_indexes())
 
 # Check requests to OpenAI
-# print('OpenAI Engines: ', openai.Engine.list())
+print('OpenAI Engines: ', openai.Engine.list())
 
-# 1. This was done 1 time to get the number of dimensions
-# text = ''
-# with open(f'contexts/1.txt') as file:
-#     text = file.read()   
+# # check if PINECONE_INDEX_NAME index already exists (only create index if not)
+if PINECONE_INDEX_NAME not in pinecone.list_indexes():
+    pinecone.create_index(PINECONE_INDEX_NAME, dimension=INDEX_DIMENSIONS)
 
-# res = openai.Embedding.create(
-#     input=[
-#         text
-#     ], engine=MODEL
-# )
-# embeds = [record['embedding'] for record in res['data']]
-
-# print('res=', res)
-# print('embeds=', embeds)
-
-# # check if 'scheppach' index already exists (only create index if not)
-# if PINECONE_INDEX_NAME not in pinecone.list_indexes():
-#     pinecone.create_index(PINECONE_INDEX_NAME, dimension=len(embeds[0]))
-
-# connect to index
+# # connect to index
 index = pinecone.Index(PINECONE_INDEX_NAME)
 
-# 2. Generate and save embeddings for our 28 txt files
-for i in range(1,29):
-    with open(f'contexts/{i}.txt') as file:
-        text = file.read()
-        print(f'\n\nProcessing file {i}.txt')
-        tokens = tokenizer.encode(text)
-        print('tokens=', len(tokens))
-        
-        # create embeddings
-        res = openai.Embedding.create(input=[text], engine=MODEL)
-        embeds = [record['embedding'] for record in res['data']]
-        print('Generated embeddings, dimensions: ', len(embeds[0]))
-        
-        # prep metadata and prepare vector payload
-        meta = {'text': text}
-        to_upsert = (
-            str(i), # vector ID
-            embeds, # vector values
-            meta) # meta (source text)
-        
-        # upsert vector to Pinecone
-        index.upsert(vectors=[to_upsert])
-        print(f'Vector #{i} was upserted OK')
+# Generate and save embeddings for our txt files
+contexts_dir_name = './contexts'
+for filename in os.listdir(contexts_dir_name):
+    if filename.endswith('.txt'):
+        filepath = os.path.join(contexts_dir_name, filename)
+        with open(filepath, 'r') as file:
+            text = file.read()
+            print(f'\n\nProcessing file {filepath}')
+            tokens = tokenizer.encode(text)
+            print('tokens=', len(tokens))
+            
+            # create embeddings
+            res = openai.Embedding.create(input=[text], engine=MODEL)
+            embeds = [record['embedding'] for record in res['data']]
+            print('Generated embeddings, dimensions: ', len(embeds[0]))
+            
+            # prep metadata and prepare vector payload
+            meta = {'text': text}
+            to_upsert = (
+                filename, # vector ID
+                embeds, # vector values
+                meta) # meta (source text)
+            
+            # upsert vector to Pinecone
+            index.upsert(vectors=[to_upsert])
+            print(f'Vector #{filename} was upserted OK')
---
+++ embeddings/keys_example.py
@@ -0,0 +1,5 @@
+OPENAI_ORG=<find on openai.com>
+OPENAI_KEY=<find on openai.com>
+
+PINECONE_API_KEY=<find on pinecone.io>
+PINECONE_INDEX_NAME=<your index name>
